8ed91209f80a18774d0585bc36031298f8fca510
fontaine pierre <pierre.ftn64@gmail.com>
2016-09-10 13:40:15 +0200

    first css and meta added

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+    <!-- Latest compiled and minified CSS -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	  <!-- Optional theme -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	  <!-- Latest compiled and minified JavaScript -->
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="global">
+      <!-- Header -->
+      <div class="header">
+        <div class="banner"></div>
+        <div class="menu">
+          <ul>
+            <li>Accueil</li>
+            <li>Projets</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        petit test des familles
+      </div>
+
+      <div class="footer">
+
+      </div>
+    </div>
+  </body>
+</html>
