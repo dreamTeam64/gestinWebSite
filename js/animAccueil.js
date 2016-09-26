20069f04d9a7847e341e616a76a3927d5f7f9b6a
fontaine pierre <pierre.ftn64@gmail.com>
2016-09-26 22:19:14 +0200

    anim improved

--- js/animAccueil.js
+++ js/animAccueil.js
@@ -1,6 +1,19 @@
 $(function(){
-  setTimeout(function(){
-    $('span[class="active"]').hide("slow");
-    $('span[class="inactive"]').show("slow");
-  },5000);
+  moveText();
 });
+
+var moveText = function(){
+  setTimeout(function(){
+    $('span[class="active"]').fadeOut();
+    $('span[class="active"]').removeClass("active");
+    $('span[class="active"]').addClass("inactive");
+    setTimeout(function(){
+      $('span[class="inactive"]').fadeIn();
+      $('span[class="inactive"]').removeClass("inactive");
+      $('span[class="inactive"]').addClass("active");
+      moveText();
+    },1000);
+
+  },5000);
+
+}
