7ac2af5e365f356227c674f14bd411ad69932194
fontaine pierre <pierre.ftn64@gmail.com>
2016-10-01 00:15:39 +0200

    init pages

--- contact.php
+++ contact.php
@@ -3,8 +3,40 @@
   <head>
     <meta charset="utf-8">
     <title>contact</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+    <!-- Latest compiled and minified CSS -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	  <!-- Optional theme -->
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	  <!-- Latest compiled and minified JavaScript -->
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/ad345ec97a.js"></script>
+    <script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+
+    </script>
   </head>
   <body>
+    <div class="global">
+      <!-- Header -->
+      <div class="header">
 
+        <div class="menu">
+          <ul>
+            <li><span class="glyphicon glyphicon-home" aria-hidden="true"></span><a href="./index.php">Home</a></li>
+            <li><span class="glyphicon glyphicon-cog" aria-hidden="true"></span><a href="./service.php">Services</a></li>
+            <li><span class="fa fa-cubes" aria-hidden="true"></span><a href="./produit.php">Produits</a></li>
+            <li><span class="fa fa-pie-chart" aria-hidden="true"></span><a href="./projet.php">Projets</a></li>
+            <li><span class="fa fa-commenting" aria-hidden="true"></span><a href="./news.php">News</a></li>
+            <li><span class="fa fa-quote-left" aria-hidden="true"></span><a href="./aboutus.php">About Us</a></li>
+            <li><span class="fa fa-paper-plane" aria-hidden="true"></span><a href="./contact.php">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="main">
+
+    </div>
   </body>
 </html>
+<script src="js/animAccueil.js"></script>
